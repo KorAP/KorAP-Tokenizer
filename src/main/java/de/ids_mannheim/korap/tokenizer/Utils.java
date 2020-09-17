29c8e7d2d312d289d5299e4700f06bc3bef98eed
Marc Kupietz <kupietz@ids-mannheim.de>
2020-09-17 15:21:26 +0200

    Add new command line options using picocli and sanitize code
    Usage: koraptokenizer [-hnpsV] [--force] [-ktt] [--[no-]tokens]
                          [-o=<output_fillename>] [<inputFiles>...]
    Tokenizes (and sentence splits) text input.
          [<inputFiles>...]   input files
          --force             Force overwrite (default: false)
      -h, --help              Show this help message and exit.
          -ktt                Deprecated. For internal use only. (default: false)
      -n, --normalize         Normalize tokens (default: false)
          --[no-]tokens       Print tokens (default: true)
      -o, --output-file=<output_fillename>
                              Output file (default: -)
      -p, --positions         Print token start and end positions as character
                                offsets (default: false)
      -s, --sentence-boundaries
                              Print sentence boundary positions (default: false)
      -V, --version           Print version information and exit.
    
    Change-Id: Ib92678c832a2d95799a8f503c3e86dd4da2b4d73

---
+++ src/main/java/de/ids_mannheim/korap/tokenizer/Utils.java
@@ -0,0 +1,29 @@
+package de.ids_mannheim.korap.tokenizer;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.AccessDeniedException;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.Files;
+
+public class Utils {
+    public static File createFile(String fname, boolean force_overwrite) {
+        File f = new File(fname);
+        try {
+            Files.createFile(f.toPath());
+        } catch (AccessDeniedException e) {
+            final String message = "ERROR: Cannot write file '" + fname + "'";
+            System.err.println(message);
+            System.exit(-1);
+        } catch (FileAlreadyExistsException e) {
+            if (!force_overwrite) {
+                final String message = "ERROR: '" + fname + "' already exits. Use --force to overwrite";
+                System.err.println(message);
+                System.exit(-1);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return f;
+    }
+}
