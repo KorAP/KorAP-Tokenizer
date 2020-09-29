383df1cbaa23951af7d8c7e8c5b7b1b0b87404a7
Marc Kupietz <kupietz@ids-mannheim.de>
2020-09-29 22:19:38 +0200

    List available KorapTokenizerImplementation options for -T
    Change-Id: I8e45410f67decb6b44fc9574dfe94c89b7abd0cc

--- src/main/java/de/ids_mannheim/korap/tokenizer/Main.java
+++ src/main/java/de/ids_mannheim/korap/tokenizer/Main.java
@@ -1,5 +1,8 @@
 package de.ids_mannheim.korap.tokenizer;
 
+import io.github.classgraph.ClassGraph;
+import io.github.classgraph.ClassInfoList;
+import io.github.classgraph.ScanResult;
 import picocli.CommandLine;
 
 import java.io.*;
@@ -7,13 +10,33 @@ import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.Callable;
+import java.util.stream.Collectors;
 
 @CommandLine.Command(mixinStandardHelpOptions = true,
         name = "koraptokenizer", version = "{}", description = "Tokenizes (and sentence splits) text input.")
 public class Main implements Callable<Integer> {
 
-    @CommandLine.Option(names = {"-T",  "--tokenizer-class"}, description = "Class name of the actual tokenizer that will be used (default: ${DEFAULT-VALUE})")
+    static class AvailableKorapTokenizerList extends ArrayList<String> {
+        AvailableKorapTokenizerList() {
+            super(listKorAPTokenizerImplementations());
+        }
+
+        static List<String> listKorAPTokenizerImplementations() {
+            List<String> korapTokenizerClassNames;
+            try (ScanResult scanResult = new ClassGraph().enableAllInfo().acceptPackages("*")
+                    .scan()) {
+                ClassInfoList korapTokenizerClasses = scanResult.getClassesImplementing("de.ids_mannheim.korap.tokenizer.KorapTokenizer");
+                korapTokenizerClassNames = korapTokenizerClasses.getNames();
+            }
+            return korapTokenizerClassNames;
+        }
+    }
+
+    @CommandLine.Option(names = {"-T", "--tokenizer-class"},
+            completionCandidates= AvailableKorapTokenizerList.class,
+            description = "Class name of the actual tokenizer that will be used (candidates: ${COMPLETION-CANDIDATES} default: ${DEFAULT-VALUE})")
     String tokenizerClassName = DerekoDfaTokenizer_de.class.getName();
 
     @CommandLine.Option(names = {"--no-tokens"}, negatable = true, description = "Print tokens (default: ${DEFAULT-VALUE})")
